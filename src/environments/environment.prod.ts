a13cf9e64498df18b4ad6977e6e80f4565ddc18c
Rael CALITRO <rael06@hotmail.fr>
2025-01-23 12:49:04 +0100

    update API endpoint for production environment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,6 @@
 export const environment = {
   production: true,
-  api: "https://numberdetection.herokuapp.com/api/test/",
-  //api: 'https://handwritten-digits-calculator.azurewebsites.net/api/Operation'
+  // api: "https://numberdetection.herokuapp.com/api/test/",
+  // api: 'https://handwritten-digits-calculator.azurewebsites.net/api/Operation',
+  api: "https://number-detection-api.rael-calitro.ovh/api/getOperationResults/",
 };
